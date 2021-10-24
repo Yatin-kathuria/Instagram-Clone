8a7c8d9aaa9cda310389e3324c82e164e8fd4acf
Yatin <yatinkathuria74@gmail.com>
2021-10-24 14:55:41 +0530

    bug fix

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styles from "./Input.module.css";
 
+const inputWrapper = {
+  border: "1px solid #dbdbdb",
+  height: "38px",
+  marginBottom: "10px",
+  borderRadius: "4px",
+};
+
 const Input = ({ value, placeholder, onChange, type }) => {
   return (
-    <div className={styles.inputWrapper}>
+    <div className={styles.inputWrapper} style={inputWrapper}>
       {value && <p className={styles.inputLabel}>{placeholder}</p>}
       <input
         type={type}
